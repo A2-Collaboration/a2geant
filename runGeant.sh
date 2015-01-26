4e028c268b1a90ffe80795fde26a314af9f3c858
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-01-26 12:01:09 +0100

    Added optional parameter passing to runGeant.sh

--- runGeant.sh
+++ runGeant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: ${0} <pluto-file.root> <output-file.root>"
+if [ $# -lt 2 ]; then
+    echo "Usage: ${0} <pluto-file.root> <output-file.root> [optional other paramters of A2 geant]"
     exit 1;
 fi
 
@@ -9,5 +9,6 @@ INFILE=$(readlink -f "${1}")
 OUTFILE=$(readlink -f "${2}")
 PROG=$(readlink -f "${0}")
 BASE=$(dirname "${PROG}")
+shift 2
 cd "${BASE}"
-exec ./A2 --mac=sim_pluto.mac --if="${INFILE}" --of="${OUTFILE}"
+exec ./A2 --mac=sim_pluto.mac --if="${INFILE}" --of="${OUTFILE}" $* 
