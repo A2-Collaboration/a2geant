24714dd559b66677c472cde6ed46c43328e0fcc6
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-03 06:38:16 +0100

    Add modifyMacro.pl to edit detector macro files (change target length on the fly, for example)

---
+++ modifyMacro.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+die "Supply regex replacement as argument." unless @ARGV == 1;
+my $regex = $ARGV[0];
+die "Regex replacement should start with s." unless $regex =~ m/^s/;
+
+my $n_lines_changed = 0;
+while(my $line = <STDIN>) {
+  $n_lines_changed++ if eval '$line =~ '.$regex;
+  die $@ if $@;
+  print $line;
+}
+
+die "$n_lines_changed != 1 Lines were changed." unless $n_lines_changed == 1;
