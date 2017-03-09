3142a2feab731830f3da6525644ff311a79b977a
sjdkay <sjdkay@gmail.com>
2017-03-09 13:20:01 +0000

    Temporarily removed PID3 PMTs and support to try and avoid issue with volume overlaps

--- src/A2DetPID3.cc
+++ src/A2DetPID3.cc
@@ -60,22 +60,25 @@ G4VPhysicalVolume* A2DetPID3::Construct1(G4LogicalVolume* MotherLogical,G4double
   fpid_xl2=2*(fpid_rout-(fpidendL*tan(30*CLHEP::deg)))*tan(fpid_theta/2);//long length at new inner radius
 
   //Make the light guide shape
-  MakeLightGuide1(); // Makes the bent light guides for the smaller PID case
-  MakePhotomultipliers(); // This is unchanged since last two PIDs
+  //MakeLightGuide1(); // Makes the bent light guides for the smaller PID case
+  //MakePhotomultipliers(); // This is unchanged since last two PIDs
 
   MakeSingleDetector1();
 
-  MakeSupports1(); // New support structure
+  //MakeSupports1(); // New support structure
 
   //Make PID Logical Volume
   //Take the centre radius from the scintillators, thickness from the lightguides~1CLHEP::cm, and length from scintillators+lightguides+pmts+base
   // G4double moth_rin=fpid_rin+fpid_thick/2-8*mm;
   G4double moth_rin=(fpid_rin - 4.05*CLHEP::mm)-0.1*CLHEP::mm; //aluminium ring
   //  G4double moth_rout=fpid_rin+fpid_thick/2+0.55*CLHEP::cm;
-  G4double moth_rout=(66*CLHEP::mm); //aluminium ring
-  G4double moth_z=fpid_z+flg_z-flg12_z+fpmt_z*2+fbase_z*2+10*CLHEP::mm + fpidendL;//extra 6mm for supports
-  fzpos=((fpid_z-moth_z)/2+6*CLHEP::mm)+fpidendL;//zposition of centre of pid relative to mother, 3mm is for support ring
-  fpmtr_z=fzpos+fpid_z/2+flg_z-flg12_z+2*fpmt_z+2*fbase_z-5/2*CLHEP::mm;//zposition of the pmt supportring
+  //G4double moth_rout=(66*CLHEP::mm); //aluminium ring Original values with supports
+  //G4double moth_z=fpid_z+flg_z-flg12_z+fpmt_z*2+fbase_z*2+10*CLHEP::mm + fpidendL;//extra 6mm for supports
+  G4double moth_rout=(38*CLHEP::mm);
+  G4double moth_z=fpid_z+5*CLHEP::mm;//extra 6mm for supports
+  //fzpos=((fpid_z-moth_z)/2+6*CLHEP::mm)+fpidendL;//zposition of centre of pid relative to mother, 3mm is for support ring
+  fzpos=2.5*CLHEP::mm;//zposition of centre of pid relative to mother
+  //fpmtr_z=fzpos+fpid_z/2+flg_z-flg12_z+2*fpmt_z+2*fbase_z-5/2*CLHEP::mm;//zposition of the pmt supportring
   G4RotationMatrix *Mrot=new G4RotationMatrix();
   Mrot->rotateY(180*CLHEP::deg);//PID III is positioned in same orientation as PID II
   G4Tubs *PIDMother=new G4Tubs("PIDD",moth_rin-(fpidendL*tan(30*CLHEP::deg)),moth_rout,moth_z/2,0*CLHEP::deg,360*CLHEP::deg);
@@ -113,13 +116,13 @@ void A2DetPID3::MakeDetector1(){
     //Check the hit positions, should collate with AcquRoot setup
     //G4cout<<"PID "<<i<<" "<<xpos<< " "<<ypos<<" "<<" "<<pid_R<<" "<<fzpos<<" "<<dpos1.phi()/CLHEP::deg<<G4endl;
     fPIDPhysi[i]=new G4PVPlacement(Rot[i],dpos1,fPIDLogic,"PID",fMyLogic,false,i+1);
-     fLGPhysi[i]=new  G4PVPlacement(Rot[i],G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z).rotateZ(fpid_theta/2),fLGLogic,"LG",fMyLogic,false,i);
-    fBASEPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+2*fpmt_z+fbase_z).rotateZ(fpid_theta/2),fBASELogic,"BASE",fMyLogic,false,i);
-     fTPMTPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+fpmt_z).rotateZ(fpid_theta/2),fTPMTLogic,"TPMT",fMyLogic,false,i);
-      fMUMEPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+fpmt_z+fbase_z).rotateZ(fpid_theta/2),fMUMELogic,"MUME",fMyLogic,false,i);
-    RotPMTR[i]=new G4RotationMatrix();
-    RotPMTR[i]->rotateZ(pid_angle);
-    new G4PVPlacement(RotPMTR[i],G4ThreeVector(0,0,fpmtr_z),fPMTRLogic,"PMTR",fMyLogic,false,i);
+     //fLGPhysi[i]=new  G4PVPlacement(Rot[i],G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z).rotateZ(fpid_theta/2),fLGLogic,"LG",fMyLogic,false,i);
+    //fBASEPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+2*fpmt_z+fbase_z).rotateZ(fpid_theta/2),fBASELogic,"BASE",fMyLogic,false,i);
+     //fTPMTPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+fpmt_z).rotateZ(fpid_theta/2),fTPMTLogic,"TPMT",fMyLogic,false,i);
+      //fMUMEPhysi[i]=new  G4PVPlacement(0,G4ThreeVector(xpos2,ypos2,fzpos+fpid_z/2+flg_z-flg12_z+fpmt_z+fbase_z).rotateZ(fpid_theta/2),fMUMELogic,"MUME",fMyLogic,false,i);
+    //RotPMTR[i]=new G4RotationMatrix();
+    //RotPMTR[i]->rotateZ(pid_angle);
+    //new G4PVPlacement(RotPMTR[i],G4ThreeVector(0,0,fpmtr_z),fPMTRLogic,"PMTR",fMyLogic,false,i);
 
  }
 
